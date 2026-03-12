4321f21d0663fe7fafc54711fb2a6eee70c515aa
Crash <1903291+hex22a@users.noreply.github.com>
2026-03-12 14:51:24 +0400

    chore: Updated import

--- backend/src/auth/strategies/HandleCallStrategy.ts
+++ backend/src/auth/strategies/HandleCallStrategy.ts
@@ -1,8 +1,9 @@
 import type { RequestHandler } from '@/src/controllers/types';
 import type { IHandleStrategy } from './IHandleStrategy';
+import type { BunRequest, MaybePromise } from 'bun';
 
 export class HandleCallStrategy implements IHandleStrategy {
-    handle(handler: RequestHandler, request: Bun.BunRequest): Bun.MaybePromise<Response> {
+    handle(handler: RequestHandler, request: BunRequest): MaybePromise<Response> {
         return handler(request);
     }
 }
