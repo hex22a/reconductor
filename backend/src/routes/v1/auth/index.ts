be671bd9c55a7c1f5cd109f782c744f2e3124ba8
Crash <1903291+hex22a@users.noreply.github.com>
2026-03-12 20:26:26 +0400

    feat: Added logout route

--- backend/src/routes/v1/auth/index.ts
+++ backend/src/routes/v1/auth/index.ts
@@ -1,9 +1,11 @@
 import createRegisterRoutes from './register';
 import createLoginRoutes from './login';
 import { createMeRoutes } from './me';
+import { createLogoutRoutes } from './logout';
 
 const registerRoutes = createRegisterRoutes();
 const loginRoutes = createLoginRoutes();
+const logoutRoutes = createLogoutRoutes();
 const meRoutes = createMeRoutes();
 
-export default { ...registerRoutes, ...loginRoutes, ...meRoutes };
+export default { ...registerRoutes, ...loginRoutes, ...logoutRoutes, ...meRoutes };
