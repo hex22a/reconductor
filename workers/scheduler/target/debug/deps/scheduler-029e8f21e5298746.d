5587f94bfc24ae01bbb84ee541b6573afaffa73c
Crash <1903291+hex22a@users.noreply.github.com>
2026-04-03 15:40:33 +0400

    fix: remove target from rust build

--- workers/scheduler/target/debug/deps/scheduler-029e8f21e5298746.d
+++
@@ -1,5 +0,0 @@
-/Users/hex22a/src/porn/reconductor/workers/scheduler/target/debug/deps/scheduler-029e8f21e5298746.d: src/main.rs
-
-/Users/hex22a/src/porn/reconductor/workers/scheduler/target/debug/deps/libscheduler-029e8f21e5298746.rmeta: src/main.rs
-
-src/main.rs:
