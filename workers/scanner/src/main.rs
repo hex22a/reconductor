cd68693699082c9cf2182be0f9b6a9d3a4fff2b3
Crash <1903291+hex22a@users.noreply.github.com>
2026-04-07 19:25:48 +0400

    chore: Updated main fnuction

--- workers/scanner/src/main.rs
+++ workers/scanner/src/main.rs
@@ -1,7 +1,15 @@
 mod nmap;
 mod queue;
 mod db;
+mod config;
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let config = config::Config::from_env()?;
+
+    let db = db::init_db(&config.database_url).await;
     println!("Hello, world!");
+    Ok(())
 }
