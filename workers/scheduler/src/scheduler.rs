d96e540f56b4ffdd082409ca767b68e712c59d53
Crash <1903291+hex22a@users.noreply.github.com>
2026-04-12 17:33:37 +0400

    chore: Added happy path test

--- workers/scheduler/src/scheduler.rs
+++ workers/scheduler/src/scheduler.rs
@@ -75,11 +75,11 @@ impl<R: ScanRepository, P: ScanPublisher, U: Utils> Scheduler<R, P, U> {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::{Arc, Mutex};
+    use std::{str::FromStr, sync::{Arc, Mutex}};
 
     use anyhow::Ok;
     use crate::db::scan::{DueScan, ScanRepository};
-    use sqlx::types::{ipnetwork::IpNetwork, time::OffsetDateTime};
+    use sqlx::types::{ipnetwork::{IpNetwork, Ipv4Network}, time::OffsetDateTime};
     use uuid::Uuid;
 
     use crate::{queue::publisher::ScanPublisher, scheduler::utils::Utils};
@@ -167,5 +167,55 @@ mod tests {
         assert_eq!(update_next_run_calls.lock().unwrap().len(), 0);
         assert_eq!(calculate_next_run_calls.lock().unwrap().len(), 0);
         assert_eq!(publish_calls.lock().unwrap().len(), 0);
+    }
+
+    #[tokio::test]
+    async fn test_poll_happy_path() {
+        // Arrange
+        let expected_scan_id: Uuid = Uuid::now_v7();
+        let expected_target: IpNetwork = IpNetwork::V4(Ipv4Network::from_str("192.168.0.0/16").unwrap());
+        let expected_schedule: String = String::from_str("5 * * * *").unwrap();
+        let expected_poll_interval_secs: u64 = 30;
+        let expected_next_run: OffsetDateTime = OffsetDateTime::now_utc();
+        let expected_due_scans: Vec<DueScan> = vec![
+            DueScan {
+                id: expected_scan_id,
+                target: expected_target,
+                schedule: Some(expected_schedule),
+            }
+        ];
+        let fetch_due_scans_calls: Arc<Mutex<Vec<()>>> = Arc::new(Mutex::new(vec![]));
+        let update_next_run_calls: Arc<Mutex<Vec<(Uuid, OffsetDateTime)>>> = Arc::new(Mutex::new(vec![]));
+        let publish_calls: Arc<Mutex<Vec<(Uuid, IpNetwork)>>> = Arc::new(Mutex::new(vec![]));
+        let calculate_next_run_calls: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
+        let mock_scan_repository = MockScanRepository {
+            due_scans: expected_due_scans,
+            fetch_due_scans_calls: fetch_due_scans_calls.clone(),
+            update_next_run_calls: update_next_run_calls.clone(),
+        };
+        let mock_scan_publisher = MockScanPublisher {
+            publish_calls: publish_calls.clone(),
+        };
+        let mock_utils = MockUtils {
+            return_value: expected_next_run,
+            calculate_next_run_calls: calculate_next_run_calls.clone(),
+        };
+        let scheduler = Scheduler::new(
+            mock_scan_repository,
+            mock_scan_publisher,
+            mock_utils,
+            expected_poll_interval_secs,
+        );
+        // Act
+        let actual_result = scheduler.poll().await.unwrap();
+
+        // Assert
+        assert_eq!(actual_result, ());
+        assert_eq!(fetch_due_scans_calls.lock().unwrap().len(), 1);
+        assert_eq!(update_next_run_calls.lock().unwrap().len(), 1);
+        assert_eq!(update_next_run_calls.lock().unwrap()[0], (expected_scan_id, expected_next_run));
+        assert_eq!(calculate_next_run_calls.lock().unwrap().len(), 1);
+        assert_eq!(publish_calls.lock().unwrap().len(), 1);
+        assert_eq!(publish_calls.lock().unwrap()[0], (expected_scan_id, expected_target));
     }
 }
