0bace0df67b066c631cf9c73744dceedf5f5ba09
Crash <1903291+hex22a@users.noreply.github.com>
2026-04-13 22:25:10 +0400

    feat: Wired all the scanner code together

--- workers/scanner/src/main.rs
+++ workers/scanner/src/main.rs
@@ -3,13 +3,29 @@ mod queue;
 mod db;
 mod config;
 
+use crate::db::scan::PgScanRepository;
+use tracing::info;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
 
     let config = config::Config::from_env()?;
+    let conn = lapin::Connection::connect(
+        &config.rabbitmq_url,
+        lapin::ConnectionProperties::default(),
+    )
+    .await?;
+    info!("Connected to RabbitMQ");
 
+    let consume_channel = conn.create_channel().await?;
     let db = db::init_db(&config.database_url).await;
-    println!("Hello, world!");
+    let repository = PgScanRepository { db };
+
+    queue::consumer::run(repository, consume_channel).await?;
+
     Ok(())
 }
