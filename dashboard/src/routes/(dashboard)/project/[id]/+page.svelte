a4db49fcfd87d0c2eacc3cdaf2a59afee7f204a1
Crash <1903291+hex22a@users.noreply.github.com>
2026-07-09 18:06:33 +0400

    chore: Added project details page

---
+++ dashboard/src/routes/(dashboard)/project/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageProps } from './$types';
+  let { data }: PageProps = $props();
+  let date = new Date(data.created_at!).toUTCString();
+</script>
+
+<div class="font-special">Project details</div>
+<div>Name: {data.name}</div>
+<div>Created At: {date}</div>
