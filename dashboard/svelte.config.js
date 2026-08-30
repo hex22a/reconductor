fe9f78457a5098a9fbd1e65aa276c4c4e4e19015
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-30 18:21:59 +0400

    chore: Updated dashboard deps

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -11,9 +11,9 @@ const config = {
         adapter: adapter({
             fallback: 'index.html',
         }),
-    },
-    alias: {
-        '@': './src',
+        alias: {
+            '@': './src',
+        },
     },
 };
 
