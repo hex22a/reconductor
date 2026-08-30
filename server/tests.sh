9d0cfd10a02d941ca9e34a01cb4376d6b49423dc
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-30 15:37:59 +0400

    chore: export env vars in script

--- server/tests.sh
+++ server/tests.sh
@@ -16,6 +16,10 @@ fi
 
 trap "${ENGINE} compose -f ../docker-compose.test.yml down -v;" EXIT;
 
+export REDIS_PASSWORD
+export DATABASE_URL
+export REDIS_URL
+
 ${ENGINE} compose -f ../docker-compose.test.yml up -d;
 cargo sqlx migrate run --source ../migrations
 cargo test
