8f762aa4eaf6480a5d4402b8a5f51d52e5acbca5
Crash <1903291+hex22a@users.noreply.github.com>
2026-09-08 21:15:39 +0400

    chore: Instrumental build

--- server/tests.sh
+++ server/tests.sh
@@ -18,6 +18,7 @@ trap "${ENGINE} compose -f ../docker-compose.test.yml down -v;" EXIT;
 
 ${ENGINE} compose -f ../docker-compose.test.yml up -d;
 echo "running migrations"
+echo $DATABASE_URL
 cargo sqlx migrate run --source ../migrations
 echo "migrations complete"
 cargo test
