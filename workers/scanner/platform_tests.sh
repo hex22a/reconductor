f2b2ae42051d7a03bf91441c0dab28775cca97f1
Crash <1903291+hex22a@users.noreply.github.com>
2026-09-08 20:21:37 +0400

    chore: Removed extra line in test script

--- workers/scanner/platform_tests.sh
+++ workers/scanner/platform_tests.sh
@@ -18,7 +18,6 @@ fi
 
 trap "${ENGINE} compose -f ../../docker-compose.test.yml down -v;" EXIT;
 
-
 ${ENGINE} compose -f ../../docker-compose.test.yml up -d;
 cargo sqlx migrate run --source ../../migrations
 cargo test --tests
